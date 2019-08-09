955737efedb6e911c4ad3ccf92ea04ca8d0b0cc5
Stephen Callum <stephen.callum@hotmail.co.uk>
2019-08-09 19:01:57 +0100

    Basic structure for portfolio page
    Will start to work on projects now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { FooterSectionComponent } from './components/footer-section/footer-section.component';
+import { SkillsSectionComponent } from './components/skills-section/skills-section.component';
+import { ProjectsSectionComponent } from './components/projects-section/projects-section.component';
+import { WelcomeSectionComponent } from './components/welcome-section/welcome-section.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeSectionComponent, pathMatch: 'full'},
+  { path: 'projects', component: ProjectsSectionComponent },
+  { path: 'skills', component: SkillsSectionComponent },
+  { path: 'footer', component: FooterSectionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { WelcomeSectionComponent } from './components/welcome-section/welcome-section.component';
+import { ProjectsSectionComponent } from './components/projects-section/projects-section.component';
+import { SkillsSectionComponent } from './components/skills-section/skills-section.component';
+import { FooterSectionComponent } from './components/footer-section/footer-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    WelcomeSectionComponent,
+    ProjectsSectionComponent,
+    SkillsSectionComponent,
+    FooterSectionComponent
   ],
   imports: [
     BrowserModule,
