9c5bee2513b78afd2df5d9faab0e28ccf51007c3
Stephen Callum <stephen.callum@hotmail.co.uk>
2019-10-03 17:12:49 +0100

    Draft for portfolio page complete

--- src/app/components/welcome-section/welcome-section.component.ts
+++ src/app/components/welcome-section/welcome-section.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { default as ParticlesConfig } from 'src/assets/particle-js/config/titleConfig/particlesjs-config.json';
+// import { default as ParticlesConfig } from 'src/assets/particle-js/config/titleConfig/particlesjs-config.json';
 
 // ParticlesJS declaration
 declare var particlesJS: any;
@@ -14,8 +14,8 @@ export class WelcomeSectionComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    particlesJS('particles-js', ParticlesConfig, function() {
-      console.log('particles.js config loaded...');
-    });
+    // particlesJS('particles-js', ParticlesConfig, function() {
+    //   console.log('particles.js config loaded...');
+    // });
   }
 }
